7f4f5b53748fd53b3de6de9c9db048ad77e309b0
Junaida15 <s364589@oslomet.no>
2023-04-27 11:56:31 +0200

    RIMZZZ

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ import sys
 
 
 # I integer (unsigned long) = 4bytes and H (unsigned short integer 2 bytes)
-# see the struct official page for more info
+# see the struct official page for more info SARIMMSKPPPP
 
 header_format = '!IIHH'
 
