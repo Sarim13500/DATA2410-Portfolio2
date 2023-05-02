302044a9d51d5fdf371adc5e2b341dc30ed22b03
Sarim13500 <sarimasher@hotmail.com>
2023-05-02 13:40:57 +0200

    Every method is improved and added comments.

--- application.py
+++ application.py
@@ -254,7 +254,7 @@ def threeWayHandshakeClient(client_socket, address):
 
 
 
-def stop_and_wait_client(client_socket, addresse, fil):
+def stop_and_wait_client(client_socket, addresse, fil, test_case):
 
     packet_str = 1460
     packets = {}
@@ -268,6 +268,10 @@ def stop_and_wait_client(client_socket, addresse, fil):
         while kjor==1:
 
             data = file.read(packet_str)
+
+            if test_case == "loss" and packet_num ==1:
+                packet_num += 1
+
             packet_name = f'packet{packet_num}'
             packets[packet_name] = create_packet(packet_num,0,0,0,data)
             client_socket.sendto(packets[f'packet{packet_num}'], addresse)
@@ -300,7 +304,7 @@ def stop_and_wait_client(client_socket, addresse, fil):
 
 
 
-def stop_and_wait_server(server_socket):
+def stop_and_wait_server(server_socket, test_case):
 
     kjor =1
     packet_nmr = 0
@@ -319,8 +323,10 @@ def stop_and_wait_server(server_socket):
 
         if packet_nmr+1 == header[0]:
             packet_nmr += 1
-            send_packet = create_packet(0,ack,0,0,tom.encode('utf-8'))
-            server_socket.sendto(send_packet,addresse)
+
+            if test_case != "skip_ack":
+                send_packet = create_packet(0,ack,0,0,tom.encode('utf-8'))
+                server_socket.sendto(send_packet,addresse)
             ack +=1
 
         elif packet_nmr == header[0]:
@@ -334,41 +340,56 @@ def stop_and_wait_server(server_socket):
 
 
 
-def gbn_client(client_socket, address, file):
-    packet_size = 1460
-    packets = {}
-    packet_num = 1
-    window_size = 5
-    ack_num = 0
-    base = 1
-    packets_sent = False
+def gbn_client(client_socket, address, file, test_case):
+    # Initialize variables
+    packet_size = 1460  # Maximum size of packet payload
+    packets = {}  # Dictionary to store packets to be sent
+    packet_num = 1  # Sequence number of the next packet to be sent
+    window_size = 5  # Size of the sender's window
+    ack_num = 0  # Sequence number of the last packet acknowledged by the receiver
+    base = 1  # Sequence number of the oldest unacknowledged packet
+    packets_sent = False  # Flag to indicate whether all packets have been sent
 
+    # Open the file to be sent
     with open(file, 'rb') as file:
+
+        # Loop until all packets have been sent and acknowledged
         while not packets_sent:
+
+            # Send packets in the sender's window
             for i in range(base, min(base + window_size, packet_num + 1)):
                 if i not in packets:
+                    # Read data from the file and create a packet
                     data = file.read(packet_size)
                     if not data:
+                        # All data has been read from the file
                         packets_sent = True
                         break
                     packet_name = f'packet{i}'
                     packets[packet_name] = create_packet(i, 0, 0, 0, data)
+                # Send the packet
                 client_socket.sendto(packets[f'packet{i}'], address)
 
+
+            # Wait for acknowledgments from the receiver
             try:
                 client_socket.settimeout(0.5)
                 while True:
                     response, _ = client_socket.recvfrom(2000)
                     header = response[:12]
                     header_list = parse_header(header)
+                    # If the acknowledgment is for the next expected packet
                     if header_list[1] == base:
                         ack_num = header_list[1]
                         base += 1
+                    # If the acknowledgment is for a packet in the sender's window
                     elif header_list[1] > base:
                         base = header_list[1]
+                    # If all packets have been acknowledged
                     if base > packet_num:
                         packets_sent = True
                         break
+            # If the sender times out waiting for an acknowledgment
             except timeout:
                 pass
 
@@ -378,35 +399,48 @@ def gbn_client(client_socket, address, file):
 
 
 
-def gbn_server(server_socket):
-    packet_size = 1460
-    packet_num = 1
-    ack = 1
-    base = 1
-    kjor = True
-    packets = {}
+def gbn_server(server_socket, test_case):
+    packet_size = 1460   # maximum packet size
+    packet_num = 1      # expected packet number
+    ack = 1             # expected ACK number
+    base = 1            # base packet number for GBN
+    kjor = True         # server loop flag
+    packets = {}        # dictionary for storing packets
 
     while kjor:
+        # Receive packet from client
         packet, address = server_socket.recvfrom(2000)
+
+        # Parse packet header
         header = packet[:12]
         header_list = parse_header(header)
 
+        # If the packet number is as expected, send an ACK and store the packet
         if header_list[0] == packet_num:
+            # Extract data from packet and store packet in dictionary
             data = packet[12:]
             packet_name = f'packet{packet_num}'
             packets[packet_name] = packet
-            server_socket.sendto(create_packet(packet_num, ack, 0, 0, "".encode('utf-8')), address)
+
+            # Send an ACK with the expected ACK number
+            if test_case != "skip_ack":
+                server_socket.sendto(create_packet(packet_num, ack, 0, 0, "".encode('utf-8')), address)
+
+            # Update expected packet and ACK numbers
             packet_num += 1
             ack += 1
 
+        # If the packet number is less than expected, resend the ACK
         elif header_list[0] < packet_num:
             server_socket.sendto(create_packet(header_list[0], ack, 0, 0, "".encode('utf-8')), address)
 
+        # If the received ACK number is equal to the base number, send all packets up to the next expected packet
         if header_list[1] == base:
             while f'packet{base}' in packets:
                 server_socket.sendto(packets[f'packet{base}'], address)
                 base += 1
 
+        # If the received packet size is less than the expected packet size, exit loop
         if packet_size != len(packet) - 12:
             kjor = False
 
@@ -414,8 +448,8 @@ def gbn_server(server_socket):
 
 
 
-
-def selective_repeat_server(server_socket):
+def selective_repeat_server(server_socket, test_case):
+    # Initialize variables
     buffer = {}
     buffer_size = 10
     buffer_start = 1
@@ -423,99 +457,117 @@ def selective_repeat_server(server_socket):
     kjor = 1
 
     while kjor == 1:
+        # Receive packet and parse header
         packet, address = server_socket.recvfrom(2000)
         header = packet[:12]
         header = parse_header(header)
 
+        # Check if packet is in buffer window
         if header[0] >= buffer_start and header[0] <= buffer_end:
+            # Add packet to buffer if not already in buffer
             if header[0] not in buffer:
                 buffer[header[0]] = packet
+                # If packet is the next in sequence, send all contiguous packets
                 if header[0] == buffer_start:
                     while buffer_start in buffer:
                         server_socket.sendto(buffer[buffer_start], address)
                         del buffer[buffer_start]
                         buffer_start += 1
                         buffer_end += 1
-        ack_packet = create_packet(0, header[0], 0, 0, "")
-        server_socket.sendto(ack_packet, address)
+
+        # Send ACK packet unless test case skips ACKs
+        if test_case != "skip_ack":
+            ack_packet = create_packet(0, header[0], 0, 0, "".encode('utf-8'))
+            server_socket.sendto(ack_packet, address)
 
 
 
-
-
-
-def selective_repeat_client(client_socket, address, filename):
+def selective_repeat_client(client_socket, address, filename, test_case):
+    # set packet size
     packet_size = 1460
+
+    # initialize packet dictionary, packet number, base, window size, and packets sent flag
     packets = {}
     packet_num = 1
     base = 1
     window_size = 5
     packets_sent = False
 
+    # open file to read data
     with open(filename, 'rb') as file:
+        # loop until all packets are sent
         while not packets_sent:
+            # send packets within the window size
             while packet_num < base + window_size and not packets_sent:
                 data = file.read(packet_size)
                 if data:
+
+                    if test_case =="loss" and packet_num ==1:
+                        packet_num +=1
+
+                    # create packet with packet number and data, add to packet dictionary
                     packet = create_packet(packet_num, 0, 0, 0, data)
                     packets[f'packet{packet_num}'] = packet
+                    # send packet to receiver
                     client_socket.sendto(packet, address)
                     packet_num += 1
                 else:
+                    # all packets have been sent
                     packets_sent = True
 
+            # wait for acknowledgement packets from receiver
             try:
                 client_socket.settimeout(0.5)
                 while True:
                     response, _ = client_socket.recvfrom(2000)
                     header = response[:12]
                     header_list = parse_header(header)
+                    # check if received packet is within the window
                     if header_list[1] >= base and header_list[1] <= base + window_size - 1:
+                        # delete acknowledged packet from dictionary and update base
                         if header_list[1] in packets:
                             del packets[f'packet{header_list[1]}']
                         base = header_list[1] + 1
+                    # check if all packets have been acknowledged
                     if base > packet_num - 1:
                         packets_sent = True
                         break
             except timeout:
+                # no acknowledgement received within timeout, continue sending packets
                 pass
 
 
-
-
-
-
-def DRTP_server (socket, metode):
+def DRTP_server (socket, metode, test_case):
 
     if metode == 'stopWait':
-        stop_and_wait_server(socket)
+        stop_and_wait_server(socket, test_case)
 
     elif metode == "GBN":
-        gbn_server(socket)
+        gbn_server(socket, test_case)
 
     elif metode == "SR":
-        selective_repeat_server(socket)
+        selective_repeat_server(socket, test_case)
 
 
-def DRTP_client (socket, addresse, metode, fil):
+def DRTP_client (socket, addresse, metode, fil, test_case):
 
 
     if metode == "stopWait":
-            stop_and_wait_client(socket, addresse ,fil)
+            stop_and_wait_client(socket, addresse ,fil, test_case)
 
     elif metode == "GBN":
-            gbn_client(socket, addresse, fil)
+            gbn_client(socket, addresse, fil, test_case)
 
 
     elif metode == "SR":
-            selective_repeat_client(socket, addresse, fil)
+            selective_repeat_client(socket, addresse, fil, test_case)
 
 
 
 
 
 
-def server(ip, port, reliable):
+def server(ip, port, reliable, test_case):
 
     #Oppretter Server
     sock = socket(AF_INET, SOCK_DGRAM)
@@ -553,7 +605,7 @@ def server(ip, port, reliable):
     threeWayHandshakeServer(sock)
 
 
-    DRTP_server(sock, reliable)
+    DRTP_server(sock, reliable, test_case)
 
 
 
@@ -561,7 +613,7 @@ def server(ip, port, reliable):
 
 
 
-def client(ip, port, fil, reliable):
+def client(ip, port, fil, reliable, test_case):
 
     # Åpne en UDP-socket på klienten
     client_socket = socket(AF_INET, SOCK_DGRAM)
@@ -588,7 +640,7 @@ def client(ip, port, fil, reliable):
     threeWayHandshakeClient(client_socket, address)
 
 
-    DRTP_client(client_socket, address, reliable, fil )
+    DRTP_client(client_socket, address, reliable, fil, test_case )
 
 
 
@@ -624,6 +676,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
+    #Sjekker om test_case inputen ble brukt
+    if args.test_case:
+
+        #sjekker hva inputen er og avslutter programmet hvis feil test_case ble gitt
+        if args.test_case != "loss" and args.test_case != "skip_ack":
+            print("Gi en riktig test case")
+            sys.exit()
+
+
 
 
     # Dette vil gi en feilmelding når klient og server spør samtidig om å være aktiv i et vindu
@@ -632,7 +693,7 @@ if __name__ == '__main__':
         sys.exit()
 
 
-    #server code
+    #server kode
     elif (args.server == True ):
 
         # Sjekker noen argumenter, får å se at de er korrekt
@@ -640,7 +701,8 @@ if __name__ == '__main__':
         ip_check(args.IP)
         check_port(args.port)
 
-        server(args.IP, args.port, args.reliable)
+        #starter server
+        server(args.IP, args.port, args.reliable, args.test_case)
 
 
 
@@ -648,7 +710,9 @@ if __name__ == '__main__':
     #Client kode
     elif (args.client == True):
 
+        # Sjekker noen argumenter, får å se at de er på riktig format
         ip_check(args.IP)
         check_port(args.port)
 
-        client(args.IP, args.port, args.file, args.reliable)
+        #Starter klient
+        client(args.IP, args.port, args.file, args.reliable, args.test_case)
